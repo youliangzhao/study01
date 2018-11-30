3e0b58caf1751a0e06a58f0ca797809f0f549905
youliangzhao <youliangzhao@hotmail.com>
2018-11-30 23:33:29 +0800

    test

--- app/controllers/lab1_controller.rb
+++ app/controllers/lab1_controller.rb
@@ -1,4 +1,5 @@
 class Lab1Controller < ApplicationController
+  before_action :authorized
   def index
   end
 
@@ -16,4 +17,11 @@ class Lab1Controller < ApplicationController
 
   def tax
   end
+
+  protected
+    def authorized
+      unless User.find_by(id: session[:user_id],status: 0)
+        redirect_to login_url, notice: "请登录系统"
+      end
+    end
 end
